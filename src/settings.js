f289d6c9c92221f1f61c7863eb163d0dc154c45b
Kamil Piechaczek <pomek@pomek.pl>
2016-12-01 23:07:29 +0100

    First working version. Tests will fail. Need refactoring!.

--- src/settings.js
+++ src/settings.js
@@ -1,10 +1,20 @@
+import Storage from './chrome/storage';
+import utils from './utils';
+
+const settings = new Storage();
 const elements = {
     blockPersonForm: document.getElementById('block-new-user'),
     userGroup: document.getElementById('user-profile-group'),
     userInput: document.getElementById('user-profile'),
     userError: document.getElementById('user-profile-error'),
     helpInfoTitle: document.getElementById('user-profile-help-title'),
-    helpInfo: document.getElementById('user-profile-help')
+    helpInfo: document.getElementById('user-profile-help'),
+    table: {
+        container: document.getElementById('table-user'),
+        data: document.getElementById('table-user-data'),
+        template: document.getElementById('table-user-template'),
+        empty: document.getElementById('table-empty-list'),
+    }
 };
 
 elements.helpInfoTitle.addEventListener('click', () => {
@@ -15,9 +25,31 @@ elements.helpInfoTitle.addEventListener('click', () => {
 elements.blockPersonForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
-    if (!elements.userInput.value.trim()) {
+    const userName = elements.userInput.value.trim();
+
+    if (!userName) {
         return setError(`User's profile cannot be empty.`);
     }
+
+    utils.validateName(userName)
+        .then(() => {
+            return settings.get(Storage.USERS);
+        })
+        .then((users) => {
+            users.push({
+                name: userName,
+                createdAt: utils.getCurrentDateAsString()
+            });
+
+            elements.userInput.value = '';
+
+            return settings.set(Storage.USERS, users);
+        })
+        .then(renderBlockedUsers)
+        .catch((err) => {
+            setError(err.message)
+        });
+
 });
 
 elements.userInput.addEventListener('input', () => {
@@ -26,9 +58,7 @@ elements.userInput.addEventListener('input', () => {
     }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM fully loaded and parsed");
-});
+document.addEventListener('DOMContentLoaded', renderBlockedUsers);
 
 function setError (message) {
     if (elements.userError.firstChild) {
@@ -46,4 +76,64 @@ function setError (message) {
         elements.userError.classList.add('hidden');
         elements.userError.setAttribute('aria-hidden', 'true');
     }
+}
+
+function removeUser (nickName, trElement) {
+    settings.get(Storage.USERS)
+        .then((newUsers) => {
+            const usersAfterDeletion = newUsers.filter((item) => item.name !== nickName);
+
+            return settings.set(Storage.USERS, usersAfterDeletion);
+        })
+        .then(() => {
+            trElement.remove();
+            renderBlockedUsers();
+        });
+}
+
+function renderBlockedUsers () {
+    settings.get(Storage.USERS)
+        .then((users) => {
+            while (elements.table.data.firstChild) {
+                elements.table.data.removeChild(elements.table.data.firstChild);
+            }
+
+            if (users.length === 0) {
+                elements.table.container.classList.add('hidden');
+                elements.table.empty.classList.remove('hidden');
+            } else {
+                elements.table.container.classList.remove('hidden');
+                elements.table.empty.classList.add('hidden');
+            }
+
+            fillTable(users);
+        });
+}
+
+function fillTable (users) {
+    users.forEach((userItem) => {
+        const clonedTemplate = document.importNode(elements.table.template, true);
+
+        const templateElements = {
+            name: clonedTemplate.content.querySelector('#table-user-name'),
+            date: clonedTemplate.content.querySelector('#table-user-date'),
+            remove: clonedTemplate.content.querySelector('#table-user-remove'),
+            parent: clonedTemplate.content.querySelector('#table-user-parent')
+        };
+
+        Object.keys(templateElements).forEach((elementId) => {
+            templateElements[elementId].removeAttribute('id');
+        });
+
+        templateElements.name.appendChild(new Text(userItem.name));
+        templateElements.date.appendChild(new Text(userItem.createdAt));
+
+        templateElements.remove.addEventListener('click', () => {
+            templateElements.remove.disabled = true;
+
+            removeUser(userItem.name, templateElements.parent);
+        });
+
+        elements.table.data.appendChild(clonedTemplate.content);
+    });
 }
--- src/utils.js
+++ src/utils.js
@@ -1,45 +1,106 @@
+import Storage from './chrome/storage';
+
 const utils = {
     /**
      * Returns a list of blocked users.
      *
-     * @returns {Array.<RegExp>}
+     * @returns {Promise<Array.<RegExp>>}
      */
     getBlockedUsers() {
-        const users = [
-            'NotExistingFacebookUsers' // Todo: Temproary code coverage fix.
-        ];
+        return new Storage().get(Storage.USERS)
+            .then((users) => {
+                users = users.map((item) => {
+                    const name = item.name.replace(/\./g, '\\.');
 
-        // Todo:
-        // - user id => /profile.php?id=[uid]
-        // - nick => /[nick]
+                    return new RegExp(`/${ name }|/profile\\.php\\?id=${ name }`);
+                });
 
-        return users.map((nickName) => new RegExp(`/${ nickName }`, 'i'));
+                return Promise.resolve(users);
+            });
     },
 
     /**
      * Return a list of elements that will be parsed by filters.
      *
-     * @returns {Array.<HTMLAnchorElement>}
+     * @returns {Promise<Array.<HTMLAnchorElement>>}
      */
     getElementsToFilter() {
-        const users = utils.getBlockedUsers();
+        return utils.getBlockedUsers()
+            .then((users) => {
+                const elements = Array.from(document.querySelectorAll('a[data-hovercard]'))
+                    .filter((element) => {
+                        const childNodes = element.childNodes;
 
-        return Array.from(document.querySelectorAll('a[data-hovercard]'))
-            .filter((element) => {
-                const childNodes = element.childNodes;
+                        // Facebook links contain instance of Text or instances of Comment (2x) and Text (1x).
+                        if (childNodes.length !== 1 && childNodes.length !== 3) {
+                            return false;
+                        }
 
-                // Facebook links contain instance of Text or instances of Comment (2x) and Text (1x).
-                if (childNodes.length !== 1 && childNodes.length !== 3) {
-                    return false;
-                }
+                        if (!(childNodes[0] instanceof Text) && !(childNodes[1] instanceof Text)) {
+                            return false;
+                        }
 
-                if (!(childNodes[0] instanceof Text) && !(childNodes[1] instanceof Text)) {
-                    return false;
-                }
+                        return users.some((user) => element.href.match(user));
+                    });
 
-                return users.some((user) => element.href.match(user));
+                return Promise.resolve(elements);
             });
+    },
+
+    /**
+     * Checks whether the given name can be blocked.
+     *
+     * @param {String} name
+     * @returns {Promise}
+     */
+    validateName(name) {
+        const settings = new Storage();
+
+        return settings.get(Storage.USERS)
+            .then((users) => {
+                const isBusy = users.some((item) => item.name === name);
+
+                if (isBusy) {
+                    throw new Error('Given profile is already disabled.');
+                }
+
+                if (name.match(/\s/)) {
+                    throw new Error('Profile name cannot contain white space.');
+                }
+            });
+    },
+
+    /**
+     * Returns current date as a string in format 'YYYY-MM-DD GG:HH'.
+     *
+     * @returns {String}
+     */
+    getCurrentDateAsString() {
+        const date = new Date();
+        const day = date.getDay();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        const hour = date.getHours();
+        const mins = date.getMinutes();
+
+        return `${year}-${prependChar(month)}-${prependChar(day)} ${prependChar(hour)}:${prependChar(mins)}`;
     }
 };
+
+/**
+ * @param {Number} number
+ * @param {Number} length
+ * @param {String} char
+ * @returns {String}
+ */
+function prependChar (number, length = 2, char = '0') {
+    number = number.toString();
+
+    if (number.length >= length) {
+        return number;
+    }
+
+    return new Array(length - number.length + 1).join(char) + number;
+}
 
 export default utils;
