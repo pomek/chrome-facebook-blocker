c9ec7c011b98bfb775a2d9ff0edc7c334017dfde
Kamil Piechaczek <pomek@pomek.pl>
2016-12-01 23:07:02 +0100

    Introduced Chrome.storage and simple proxy for it.

---
+++ src/chrome/storage.js
@@ -0,0 +1,37 @@
+/* global chrome */
+
+export default class Storage {
+    static get USERS () {
+        return 'users';
+    }
+
+    /**
+     * Updates the Chrome Storage.
+     *
+     * @returns {Promise}
+     */
+    set (keyOrSettings, value) {
+        const settings = (typeof value === 'undefined') ? keyOrSettings : {
+            [keyOrSettings]: value
+        };
+
+        return new Promise((resolve) => {
+            chrome.storage.sync.set(settings, resolve);
+        });
+    }
+
+    /**
+     * Reads the storage and synchronizes the values with a local config.
+     * @returns {Promise}
+     */
+    get (key) {
+        const defaultSettings = {
+            [Storage.USERS]: []
+        };
+
+        return new Promise((resolve) => {
+            chrome.storage.sync.get(defaultSettings, (items) => resolve(items[key]));
+        });
+    }
+
+}
