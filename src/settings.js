a7bfea1cdeeb71d104e2b081d01527202e24d986
Kamil Piechaczek <pomek@pomek.pl>
2016-12-02 21:51:26 +0100

    Changed names and ids.

--- src/settings.js
+++ src/settings.js
@@ -2,31 +2,53 @@ import Storage from './chrome/storage';
 import utils from './utils';
 
 const settings = new Storage();
+
 const elements = {
-    blockPersonForm: document.getElementById('block-new-user'),
-    userGroup: document.getElementById('user-profile-group'),
-    userInput: document.getElementById('user-profile'),
-    userError: document.getElementById('user-profile-error'),
-    helpInfoTitle: document.getElementById('user-profile-help-title'),
-    helpInfo: document.getElementById('user-profile-help'),
-    table: {
+    form: {
+        // Whole form.
+        root: document.getElementById('block-user-form'),
+
+        // Group of elements related to user's input.
+        profileGroup: document.getElementById('block-user-profile-group'),
+
+        // User's input.
+        profileInput: document.getElementById('block-user-profile-input'),
+
+        // Container for error.
+        profileError: document.getElementById('block-user-profile-error'),
+
+        // Label with information.
+        helpLabel: document.getElementById('block-user-help-label'),
+
+        // Information for user about user's Facebook profile.
+        helpMessage: document.getElementById('block-user-help-message')
+    },
+    template: {
+        // Whole template.
+        root: document.getElementById('table-user-template'),
+
+        // Container for blocked users.
         container: document.getElementById('table-user'),
+
+        // Element where new users will be inserted.
         data: document.getElementById('table-user-data'),
-        template: document.getElementById('table-user-template'),
-        empty: document.getElementById('table-empty-list'),
+
+        // Element which informs about empty list.
+        empty: document.getElementById('table-empty-list')
     }
 };
 
 // Show additional block with information about user's profile.
-elements.helpInfoTitle.addEventListener('click', () => {
-    elements.helpInfoTitle.classList.add('hidden');
-    elements.helpInfo.classList.remove('hidden');
+elements.form.helpLabel.addEventListener('click', () => {
+    elements.form.helpLabel.classList.add('hidden');
+    elements.form.helpMessage.classList.remove('hidden');
 });
 
-elements.blockPersonForm.addEventListener('submit', (evt) => {
+// Handle user's input when user sent a form.
+elements.form.root.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
-    const userName = elements.userInput.value.trim();
+    const userName = elements.form.profileInput.value.trim();
 
     if (!userName) {
         return setError(`User's profile cannot be empty.`);
@@ -37,12 +59,12 @@ elements.blockPersonForm.addEventListener('submit', (evt) => {
             return settings.get(Storage.USERS);
         })
         .then((users) => {
+            elements.form.profileInput.value = '';
+
             users.push({
                 name: userName,
                 createdAt: utils.getCurrentDateAsString()
             });
-
-            elements.userInput.value = '';
 
             return settings.set(Storage.USERS, users);
         })
@@ -50,76 +72,90 @@ elements.blockPersonForm.addEventListener('submit', (evt) => {
         .catch((err) => {
             setError(err.message);
         });
-
 });
 
-elements.userInput.addEventListener('input', () => {
-    if (!elements.userError.classList.contains('hidden') && elements.userInput.value.trim()) {
+// Handle user's input.
+elements.form.profileInput.addEventListener('input', () => {
+    if (
+        !elements.form.profileError.classList.contains('hidden') &&
+        elements.form.profileInput.value.trim()
+    ) {
         setError(null);
     }
 });
 
+// Load blocked users when page is ready.
 document.addEventListener('DOMContentLoaded', renderBlockedUsers);
 
+/**
+ * @param {String|null} message
+ */
 function setError (message) {
-    if (elements.userError.firstChild) {
-        elements.userError.firstChild.remove();
+    if (elements.form.profileError.firstChild) {
+        elements.form.profileError.firstChild.remove();
     }
 
     if (message) {
-        elements.userError.appendChild(new Text(message));
-        elements.userError.removeAttribute('aria-hidden');
-        elements.userError.classList.remove('hidden');
-        elements.userGroup.classList.add('has-error');
+        elements.form.profileError.appendChild(new Text(message));
+        elements.form.profileError.removeAttribute('aria-hidden');
+        elements.form.profileError.classList.remove('hidden');
+        elements.form.profileGroup.classList.add('has-error');
 
     } else {
-        elements.userGroup.classList.remove('has-error');
-        elements.userError.classList.add('hidden');
-        elements.userError.setAttribute('aria-hidden', 'true');
+        elements.form.profileGroup.classList.remove('has-error');
+        elements.form.profileError.classList.add('hidden');
+        elements.form.profileError.setAttribute('aria-hidden', 'true');
     }
 }
 
-function removeUser (nickName, trElement) {
-    settings.get(Storage.USERS)
+/**
+ * Removes user from the blocked list.
+ *
+ * @param {String} nickName
+ */
+function removeUser (nickName) {
+    return settings.get(Storage.USERS)
         .then((newUsers) => {
             const usersAfterDeletion = newUsers.filter((item) => item.name !== nickName);
 
             return settings.set(Storage.USERS, usersAfterDeletion);
-        })
-        .then(() => {
-            trElement.remove();
-            renderBlockedUsers();
         });
 }
 
+/**
+ * Render the blocked users.
+ */
 function renderBlockedUsers () {
     settings.get(Storage.USERS)
         .then((users) => {
-            while (elements.table.data.firstChild) {
-                elements.table.data.removeChild(elements.table.data.firstChild);
+            while (elements.template.data.firstChild) {
+                elements.template.data.removeChild(elements.template.data.firstChild);
             }
 
             if (users.length === 0) {
-                elements.table.container.classList.add('hidden');
-                elements.table.empty.classList.remove('hidden');
+                elements.template.container.classList.add('hidden');
+                elements.template.empty.classList.remove('hidden');
             } else {
-                elements.table.container.classList.remove('hidden');
-                elements.table.empty.classList.add('hidden');
+                elements.template.container.classList.remove('hidden');
+                elements.template.empty.classList.add('hidden');
             }
 
-            fillTable(users);
+            createSingleElement(users);
         });
 }
 
-function fillTable (users) {
+/**
+ * @param {Array} users
+ */
+function createSingleElement (users) {
     users.forEach((userItem) => {
-        const clonedTemplate = document.importNode(elements.table.template, true);
+        const template = document.importNode(elements.template.root, true);
 
         const templateElements = {
-            name: clonedTemplate.content.querySelector('#table-user-name'),
-            date: clonedTemplate.content.querySelector('#table-user-date'),
-            remove: clonedTemplate.content.querySelector('#table-user-remove'),
-            parent: clonedTemplate.content.querySelector('#table-user-parent')
+            name: template.content.querySelector('#table-user-name'),
+            date: template.content.querySelector('#table-user-date'),
+            remove: template.content.querySelector('#table-user-remove'),
+            parent: template.content.querySelector('#table-user-parent')
         };
 
         Object.keys(templateElements).forEach((elementId) => {
@@ -132,9 +168,10 @@ function fillTable (users) {
         templateElements.remove.addEventListener('click', () => {
             templateElements.remove.disabled = true;
 
-            removeUser(userItem.name, templateElements.parent);
+            removeUser(userItem.name)
+                .then(renderBlockedUsers);
         });
 
-        elements.table.data.appendChild(clonedTemplate.content);
+        elements.template.data.appendChild(template.content);
     });
 }
